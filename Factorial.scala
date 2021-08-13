5175c38326e786a2a479f850a5b0e6e876804d02
aodiogo <diogo.oliveira@gmail.com>
2021-08-12 22:19:30 -0400

    Add more Scala commands.

---
+++ Factorial.scala
@@ -0,0 +1,11 @@
+object Factorial {
+  def main(args: Array[String]) {
+    def factorial(n: Int): BigInt = {
+      if(n < 2) 1
+      else n*factorial(n-1)
+    }
+
+    println("Factorial of 6 is: " + factorial(6));
+
+  }
+}
---
+++ RandLetters.scala
@@ -0,0 +1,7 @@
+object RandLetters {
+  def main(args: Array[String]) {
+    val name = "Diogo"
+    for(i <- 0 until name.length) println(name(i))
+  }
+
+}
