197696ffe156d1fe8fc29f684f21b625e58d70f4
aodiogo <diogo.oliveira@gmail.com>
2021-08-26 23:34:51 -0400

    Update TwoSum.scala

--- TwoSum.scala
+++ TwoSum.scala
@@ -2,21 +2,23 @@ object Done extends Exception {}
 
 object TwoSum extends App {
   val input = Array(7,5,11,9)
-  val sum = 18
+  val sum = 16
   val numberMap = collection.mutable.Map[Int, Int]()
 
-  try {
-  for(i <- 0 until input.length) {
-    val current = input(i)
-    val pair = sum - current
-    if(numberMap.contains(pair)) {
-      println(s"Pair is " + current + "["+i+"]"+" and "+ pair+"["+ numberMap(pair) + "]")
-      throw Done
-    } else {
-      numberMap += (current -> i)
+  def twoSumCheck(): Int = {
+    for(i <- 0 until input.length) {
+      val current = input(i)
+      val pair = sum - current
+      if(numberMap.contains(pair)) {
+        println(f"Pair for sum=$sum is $current[$i] and $pair[${numberMap(pair)}]")
+        return 1
+      } else {
+        numberMap += (current -> i)
+      }
     }
+    return 0
   }
-  } catch {
-    case Done =>
-  }
+
+  if(twoSumCheck() == 1) println("Success")
+  else println("Not found")
 }
