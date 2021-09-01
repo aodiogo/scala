a60a1999bce6f285fe6e55bf2c54df97111fc496
aodiogo <diogo.oliveira@gmail.com>
2021-09-01 11:19:39 -0400

    Test BinarySearch.scala.

--- BinarySearch.scala
+++ BinarySearch.scala
@@ -4,20 +4,22 @@ object BinarySearch extends App {
   //Restrains the search on half the list at each recursive call.
   
   val source = List(10,20,33,99,1000,87,72,55,77)
-  val item = 10
+  var item = 10
 
   def binarySearchHelper(source:List[Int], item:Int, left:Int, right:Int):Boolean = {
     if(left > right) {
-      false
+      return false
     } else {
-      val mid:Int = (right - left)/2
+      val mid:Int = left + (right - left)/2
       val midItem = source(mid)
+      println(s"mid\tleft\tright\titem\tmidItem") 
+      println(s"${mid}\t${left}\t${right}\t${item}\t${midItem}")
       if(midItem == item) {
-        true
+        return true
       } else if(midItem > item) {
-        binarySearchHelper(source, item, left, mid-1)
+        return binarySearchHelper(source, item, left, mid-1)
       } else {
-        binarySearchHelper(source, item, left+1, right)
+        return binarySearchHelper(source, item, mid+1, right)
       }
     }
   }
@@ -26,5 +28,29 @@ object BinarySearch extends App {
     binarySearchHelper(source, item, 0, source.length -1)     
   }
 
-  println(s"${source.toString} contains ${item}? ${binarySearch(source.sortWith(_<_), item)}")
+  val sortedSource = source.sortWith(_<_)
+
+  println(s"${sortedSource.toString} contains ${item}? ${binarySearch(sortedSource, item)}")
+
+  item = 1000
+
+  println(s"${sortedSource.toString} contains ${item}? ${binarySearch(sortedSource, item)}")
+
+  item = 99
+
+  println(s"${sortedSource.toString} contains ${item}? ${binarySearch(sortedSource, item)}")
+
+  item = 20
+
+  println(s"${sortedSource.toString} contains ${item}? ${binarySearch(sortedSource, item)}")
+
+  item = 0
+
+  println(s"${sortedSource.toString} contains ${item}? ${binarySearch(sortedSource, item)}")
+
+  item = 10000
+
+  println(s"${sortedSource.toString} contains ${item}? ${binarySearch(sortedSource, item)}")
+
+
 }
