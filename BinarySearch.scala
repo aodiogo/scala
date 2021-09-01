15c48bbb5a66342ceea695f679cacd8da48c919e
aodiogo <diogo.oliveira@gmail.com>
2021-09-01 08:55:44 -0400

    WIP: Modify BinarySearch.scala.

--- BinarySearch.scala
+++ BinarySearch.scala
@@ -4,10 +4,10 @@ object BinarySearch extends App {
   //Restrains the search on half the list at each recursive call.
   
   val source = List(10,20,33,99,1000,87,72,55,77)
-  val item = 55
+  val item = 550
 
   def binarySearch(source: List[Int], item:Int):Boolean = {
-    true
+    false 
   }
 
   println(s"${source.toString} contains ${item}? ${binarySearch(source, item)}")
