1cf5e32b1bb2574d5087d256a5d906e664faaba0
aodiogo <diogo.oliveira@gmail.com>
2021-09-01 10:53:12 -0400

    Complete BinarySearch.scala.

--- BinarySearch.scala
+++ BinarySearch.scala
@@ -4,11 +4,27 @@ object BinarySearch extends App {
   //Restrains the search on half the list at each recursive call.
   
   val source = List(10,20,33,99,1000,87,72,55,77)
-  val item = 550
+  val item = 10
 
-  def binarySearch(source: List[Int], item:Int):Boolean = {
-    false 
+  def binarySearchHelper(source:List[Int], item:Int, left:Int, right:Int):Boolean = {
+    if(left > right) {
+      false
+    } else {
+      val mid:Int = (right - left)/2
+      val midItem = source(mid)
+      if(midItem == item) {
+        true
+      } else if(midItem > item) {
+        binarySearchHelper(source, item, left, mid-1)
+      } else {
+        binarySearchHelper(source, item, left+1, right)
+      }
+    }
   }
 
-  println(s"${source.toString} contains ${item}? ${binarySearch(source, item)}")
+  def binarySearch(source: List[Int], item:Int):Boolean = {
+    binarySearchHelper(source, item, 0, source.length -1)     
+  }
+
+  println(s"${source.toString} contains ${item}? ${binarySearch(source.sortWith(_<_), item)}")
 }
