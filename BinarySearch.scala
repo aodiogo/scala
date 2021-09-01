4b34593188a2f4c179cb117430e623a063aec563
aodiogo <diogo.oliveira@gmail.com>
2021-09-01 08:52:11 -0400

    WIP: Add BinarySearch.scala.

---
+++ BinarySearch.scala
@@ -0,0 +1,14 @@
+object BinarySearch extends App {
+  //Given an ordered List of Int
+  //Returns true if a certain number is found.
+  //Restrains the search on half the list at each recursive call.
+  
+  val source = List(10,20,33,99,1000,87,72,55,77)
+  val item = 55
+
+  def binarySearch(source: List[Int], item:Int):Boolean = {
+    true
+  }
+
+  println(s"${source.toString} contains ${item}? ${binarySearch(source, item)}")
+}
