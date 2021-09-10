ab449348614b17fc9cacf76741bf8608c6a67eca
aodiogo <diogo.oliveira@gmail.com>
2021-09-10 08:25:58 -0400

    Change Array[Int] to Array[String] in Permutations.scala.

--- Permutations.scala
+++ Permutations.scala
@@ -4,13 +4,13 @@ object Permutations extends App {
   // find all permutations in array
   // given [1,2,3] -> 1,2,3 ; 1,3,2 ; 2,3,1 ; 2,1,3 ; 3,1,2 ; 3,2,1
   
-  def printArr(nums:Array[Int], n:Int):Unit = {
+  def printArr(nums:Array[String], n:Int):Unit = {
     for(i <- 0 to n-1) print(nums(i)+" ")
     println("")
   }
 
 
-  def permuteHelper(a:Array[Int], size:Int=0, n:Int):Unit = {
+  def permuteHelper(a:Array[String], size:Int=0, n:Int):Unit = {
     if(size == 1) {
       printArr(a,n)
     }
@@ -18,7 +18,7 @@ object Permutations extends App {
     for(i <- 0 to size-1) {
       permuteHelper(a, size - 1, n)
       if(size % 2 == 1) {
-        var temp:Int = a(0)
+        var temp:String = a(0)
         a(0) = a(size - 1)
         a(size - 1) = temp
       } else {
@@ -30,15 +30,15 @@ object Permutations extends App {
   }
 
 
-  def permute(nums:Array[Int]):Unit = {
+  def permute(nums:Array[String]):Unit = {
     return permuteHelper(nums, nums.length, nums.length)
   }
 
-  permute(Array(1,2,3))
+  permute(Array("a","b","c"))
 
-  permute(Array(1,2,3,4,5))
+  permute(Array("D","i","o","g","o"))
 
-  permute(Array(1,2))
+  permute(Array("A","B"))
 
 
 }
