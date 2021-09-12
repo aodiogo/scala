e2fd8080090e254bdd68cf102c522ae9c28ba7a3
aodiogo <diogo.oliveira@gmail.com>
2021-09-12 10:05:21 -0400

    Add Fibonacci.scala.

---
+++ Fibonacci.scala
@@ -0,0 +1,11 @@
+object Fibonacci extends App {
+  def fibonacci(num:Int):Int = {
+    if(num < 2) { 1
+    } else 
+    fibonacci(num-1) + fibonacci(num-2)
+  }
+  println("Fibonacci de 3:"+fibonacci(3))
+  println("Fibonacci de 4:"+fibonacci(4))
+
+  for(i <- 0 to 10) print(s"${fibonacci(i)} ")
+}
