30d04ae3c2f76f3718bd3f784217b080a725066c
aodiogo <diogo.oliveira@gmail.com>
2021-09-17 09:25:07 -0400

    Add MyMap[key,value] to Generics.scala

--- GenericsTest.scala
+++ GenericsTest.scala
@@ -2,10 +2,15 @@
 object GenericsTest extedns App {
   class MyList[A]
 
+  class MyMap[key, value]
+
   def empty[A]: MyList[A] = {
     new MyList[A]()
   }
 
+  def emptyMap[k,v]: MyMap[k,v] = {
+    new MyMap[k,v]()
+  }
 
 
 
