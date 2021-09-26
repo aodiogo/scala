a4ad7190099b730eb92bd403c6de2ca1136ec06b
Diogo Oliveira <diogo.oliveira@gmail.com>
2021-09-26 10:29:30 -0400

    GenericsExamples: add CovariantList examples

--- GenericsExamples.scala
+++ GenericsExamples.scala
@@ -3,7 +3,10 @@ object GenericsExamples extends App {
 	class Dog extends Animal
 	class Cat extends Animal
 
-	class CovariantList[+A]
+	class CovariantList[+A] {
+		def add[B >: A](element:B):CovariantList[B] = new CovariantList[B]() //B is a supertype of A
+		//ex: B is Animal, A is Cat. Then try add(Dog), returns MyList[Animal]
+	}
 
 	val animal:Animal = new Cat
 
@@ -12,4 +15,12 @@ object GenericsExamples extends App {
 	val dogList:CovariantList[Animal] = new CovariantList[Dog]	
 	
 	val catList:CovariantList[Animal] = new CovariantList[Cat]
+
+	val dog = new Dog
+
+	val dogGenericsList = catList.add(dog)
+	
+	println(dogGenericsList.getClass)
+
+	println(dogGenericsList.isInstanceOf[CovariantList[Animal]]) 
 }
