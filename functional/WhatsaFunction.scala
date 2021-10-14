24d553e6707d4d1a09599fd0cc8b4cdfff96df0f
Diogo Oliveira <diogo.oliveira@gmail.com>
2021-10-14 17:41:44 -0400

    more functional exercises

--- functional/WhatsaFunction.scala
+++ functional/WhatsaFunction.scala
@@ -19,8 +19,21 @@ object WhatsaFunction extends App {
         override def apply(a:Int, b:Int):Int = a+b
     }
 
+    val customConcat = new Function2[String,String,String] {
+        override def apply(a:String, b:String):String = a+b
+    }
+
+    trait MyFunction2[A, MyFunction[A,B], B] {
+        def apply(a:A, b:MyFunction[A,B]):B
+    }
+
+    def myFunction2(f:Int => Int, a:Int):Int = f(a) 
+    def double2(a:Int):Int = myFunction2(defaultDoubler, a)
+
     println(doubler(2))
     println(defaultDoubler(2))
     println(stringToIntConverter("4"))
     println(adder(2,2))
+    println(customConcat("2","2"))
+    println(double2(2))
 }
