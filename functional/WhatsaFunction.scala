7773ae24c2ed74143e34b31cf4cc640aa3c9c770
Diogo Oliveira <diogo.oliveira@gmail.com>
2021-10-14 16:28:27 -0400

    add Function2

--- functional/WhatsaFunction.scala
+++ functional/WhatsaFunction.scala
@@ -14,7 +14,13 @@ object WhatsaFunction extends App {
     val stringToIntConverter = new Function1[String, Int] {
         override def apply(a:String):Int = a.toInt
     }
+
+    val adder = new Function2[Int,Int,Int] {
+        override def apply(a:Int, b:Int):Int = a+b
+    }
+
     println(doubler(2))
     println(defaultDoubler(2))
     println(stringToIntConverter("4"))
+    println(adder(2,2))
 }
