69a4b819a7c21b6bfdbb464b549505ea7318f221
aodiogo <diogo.oliveira@gmail.com>
2021-10-26 14:44:46 -0400

    Add .gitignore

--- .history/functional/AnonymousFunctions_20211018121056.scala
+++
@@ -1,7 +0,0 @@
-object AnonymousFunctions extends App {
-    val doubler = new Function1[Int,Int] {
-        override def apply(x:Int) = x*2
-    }
-
-    val doublerAnonymous: (x:Int) => Int = x*2
-}
